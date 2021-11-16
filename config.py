a5f9603c0332466766c75af67deac4d35331ae19
capjamesg <jamesg@jamesg.blog>
2021-11-16 18:52:41 +0000

    add config file

---
+++ config.py
@@ -0,0 +1,56 @@
+HOST = "localhost"
+PORT = 5030
+
+ACCEPTED_METHODS = ["BREW", "POST", "GET", "PROPFIND", "WHEN", "GET"]
+ACCEPTED_COFFEE_SCHEMES = [
+    "koffie",
+    "q%C3%A6hv%C3%A6",
+    "%D9%82%D9%87%D9%88%D8%A9",
+    "akeita",
+    "koffee",
+    "kahva",
+    "kafe",
+    "caf%C3%E8",
+    "%E5%92%96%E5%95%A1",
+    "kava",
+    "k%C3%A1va",
+    "kaffe",
+    "coffee",
+    "kafo",
+    "kohv",
+    "kahvi",
+    "%4Baffee",
+    "%CE%BA%CE%B1%CF%86%CE%AD",
+    "%E0%A4%95%E0%A5%8C%E0%A4%AB%E0%A5%80",
+    "%E3%82%B3%E3%83%BC%E3%83%92%E3%83%BC",
+    "%EC%BB%A4%ED%94%BC",
+    "%D0%BA%D0%BE%D1%84%D0%B5",
+    "%E0%B8%81%E0%B8%B2%E0%B9%81%E0%B8%9F"
+]
+
+MILKS = ["cream", "half-and-half", "whole-milk", "part-skim", "skim", "oat milk", "almond milk", "soya milk", "coconut milk"]
+
+ACCEPTED_ADDITIONS = {
+    "cream": "Cream",
+    "half-and-half": "Half-and-half",
+    "whole-milk": "Whole milk",
+    "part-skim": "Part skimmed milk",
+    "skim": "Skimmed milk",
+    "non-dairy": "Non-dairy milk",
+    "vanilla": "Vanilla syrup",
+    "almond": "Almond syrup",
+    "raspberry": "Raspberry syrup",
+    "chocolate": "Chocolate syrup",
+    "whisky": "Whisky",
+    "rum": "Rum",
+    "kahlua": "Kahlua",
+    "aquavit": "Aquavit",
+    "oat milk": "Oat milk",
+    "soya milk": "Soya milk",
+    "coconut milk": "Coconut milk",
+    "almond milk": "Almond milk"
+}
+
+COFFEE_POTS = {
+    "james": "James' Coffee Pot"
+}
